c3f277bb6e24be7b5cb10baa25b14b2280697ba6
Trista Huang <trista.huang@outlook.com>
2017-02-02 15:01:34 -0800

    Fix for #1, First quote display

---
+++ application/controllers/First.php
@@ -0,0 +1,30 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class First extends Application
+{
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Homepage for our app
+	 */
+	public function index()
+	{
+		// this is the view we want shown
+		$this->data['pagebody'] = 'justone';
+
+		// get the first record, to pass on to our view
+		$record = $this->quotes->get(1);
+		$this->data['what'] = $record['what'];
+		$this->data['who'] = $record['who'];
+		$this->data['mug'] = $record['mug'];
+
+		$this->render();
+	}
+
+}
