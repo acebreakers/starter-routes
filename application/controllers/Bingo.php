a467e194c9b8dab488f918a7dcb7c77d44ff73ab
eyyu <eva.yw.yu@gmail.com>
2017-02-02 20:13:20 -0800

    Issue #7- the bingo routing issue- fixed with regular expression routing.

---
+++ application/controllers/Bingo.php
@@ -0,0 +1,28 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Bingo extends Application
+{
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Homepage for our app
+	 */
+	public function index()
+	{
+      $this->data['pagebody'] = 'justone'; 
+
+      // build the list of authors, to pass on to our view
+      $record = $this->quotes->get(5);
+
+      $this->data = array_merge($this->data, $record);
+      $this->render();
+	}
+
+
+}
